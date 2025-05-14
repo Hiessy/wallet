cd232cec4581b3c913cbd0b4fbbb7513c0d063ae
Martin Diaz <diaz.martin06@gmail.com>
2025-05-14 13:32:21 -0300

    removed unused values from request class

--- account/src/main/java/org/cyan/in/model/AccountRequest.java
+++ account/src/main/java/org/cyan/in/model/AccountRequest.java
@@ -1,5 +1,6 @@
 package org.cyan.in.model;
 
+import jakarta.validation.GroupSequence;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,25 +9,28 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 
 import jakarta.validation.constraints.*;
-import lombok.*;
-
-import java.math.BigDecimal;
 
 @Data
 @Builder(toBuilder = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@GroupSequence({ValidationGroups.First.class, ValidationGroups.Second.class, AccountRequest.class})
 public class AccountRequest {
 
     @NotBlank(message = "Alias is required")
     @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Alias must contain only letters and numbers")
     private String alias;
 
-    @NotBlank(message = "Bank name is required")
-    @Size(min = 2, message = "Bank name must be at least 2 characters long")
+    @NotBlank(message = "Bank name is required", groups = ValidationGroups.First.class)
+    @Size(min = 2, message = "Bank name must be at least 2 characters long", groups = ValidationGroups.Second.class)
     private String bankName;
 
     @NotNull(message = "Balance is required")
-    @DecimalMin(value = "0.0", inclusive = true, message = "Balance must be non-negative")
+    @DecimalMin(value = "0.0", message = "Balance must be non-negative")
     private BigDecimal balance;
+}
+
+interface ValidationGroups {
+    interface First {}
+    interface Second {}
 }
